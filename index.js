03ef992edf16c8d86c62d2658188913b4ae6aaaf
Troppy2 <138890479+Troppy2@users.noreply.github.com>
2025-04-24 20:14:21 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+//To Do
+//When user clicks one how many to purhchase---add up the number of items on an icon in the shopping cart icon.
+//fix the catagories to show the product cards when user clicks on them.
+//Send a receipt to the users email when the have pressed the pre order form and send how many they have ordered to their email.
